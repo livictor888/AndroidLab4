2596c5988687dc19f594e1ab4108a69cde0ca584
livictor888 <https://github.com/livictor888/COMP3761>
2022-10-07 11:48:41 -0700

    lab4

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.lab4;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,17 +25,33 @@ public class MainActivity extends AppCompatActivity {
         Button toRelative = findViewById(R.id.btnToRelativeLayout);
         toRelative.setTextColor(Color.parseColor("black"));
         toRelative.setBackgroundColor(Color.parseColor("magenta"));
+        toRelative.setOnClickListener(view -> {
+            Intent intent = new Intent(this, RelativeLayoutForm.class);
+            startActivity(intent);
+        });
 
         Button toLinear = findViewById(R.id.btnToLinearLayout);
         toLinear.setTextColor(Color.parseColor("black"));
         toLinear.setBackgroundColor(Color.parseColor("blue"));
+        toLinear.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LinearLayoutForm.class);
+            startActivity(intent);
+        });
 
         Button toGrid = findViewById(R.id.btnToGridLayout);
         toGrid.setTextColor(Color.parseColor("black"));
         toGrid.setBackgroundColor(Color.parseColor("red"));
+        toGrid.setOnClickListener(view -> {
+            Intent intent = new Intent(this, GridLayoutForm.class);
+            startActivity(intent);
+        });
 
         Button toTable = findViewById(R.id.btnToTableLayout);
         toTable.setTextColor(Color.parseColor("black"));
         toTable.setBackgroundColor(Color.parseColor("green"));
+        toTable.setOnClickListener(view -> {
+            Intent intent = new Intent(this, TableLayoutForm.class);
+            startActivity(intent);
+        });
     }
 }
